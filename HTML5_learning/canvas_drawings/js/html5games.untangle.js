9ddc7bdaa4bb701826dfe4a3fdcfd538b0a20b56
Eduard Luhtonen <luhtonen@gmail.com>
2011-09-15 14:32:44 +0300

    drawCircle function created

--- HTML5_learning/canvas_drawings/js/html5games.untangle.js
+++ HTML5_learning/canvas_drawings/js/html5games.untangle.js
@@ -1,41 +1,26 @@
+var untangleGame = {}
+
+function drawCircle(ctx, x, y, radius) {
+	ctx.fillStyle = "rgba(200, 200, 100, .9)";
+	ctx.beginPath();
+	ctx.arc(x, y, radius, 0, Math.PI*2, true);
+	ctx.closePath();
+	ctx.fill();
+}
 $(function() {
 	var canvas = document.getElementById("game");
 	var ctx = canvas.getContext("2d");
-	ctx.fillStyle = "rgba(200, 200, 100, .6)";
 	
-	// draw bottom half circle
-	ctx.beginPath();
-	ctx.arc(100, 110, 50, 0, Math.PI);
-	ctx.closePath();
-	ctx.fill();
+	var circleRadius = 10;
 	
-	// draw top half circle
-	ctx.beginPath();
-	ctx.arc(100, 90, 50, 0, Math.PI, true);
-	ctx.closePath();
-	ctx.fill();
+	var width = canvas.width;
+	var height = canvas.height;
 	
-	// draw left half circle
-	ctx.beginPath();
-	ctx.arc(230, 100, 50, Math.PI/2, Math.PI*3/2);
-	ctx.closePath();
-	ctx.fill();
-	
-	// draw right half circle
-	ctx.beginPath();
-	ctx.arc(250, 100, 50, Math.PI*3/2, Math.PI/2);
-	ctx.closePath();
-	ctx.fill();
-	
-	// draw a shape that is almost a circle
-	ctx.beginPath();
-	ctx.arc(180, 240, 50, Math.PI*7/6, Math.PI*2/3);
-	ctx.closePath();
-	ctx.fill();
-	
-	// draw a small arc
-	ctx.beginPath();
-	ctx.arc(150, 250, 50, Math.PI*7/6, Math.PI*2/3, true);
-	ctx.closePath();
-	ctx.fill();
+	// random 5 circles
+	var circlesCount = 5;
+	for (var i=0;i<circlesCount;i++) {
+		var x = Math.random()*width;
+		var y = Math.random()*height;
+		drawCircle(ctx, x, y, circleRadius);
+	}
 });
