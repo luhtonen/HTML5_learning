b9ad2ccf55e7fa8bc48ea7629c3a1b53c7786fa7
Eduard Luhtonen <luhtonen@gmail.com>
2011-09-16 14:13:03 +0300

    mark intersecting lines as bold

--- HTML5_learning/canvas_drawings/js/html5games.untangle.js
+++ HTML5_learning/canvas_drawings/js/html5games.untangle.js
@@ -17,6 +17,7 @@ function Line(startPoint,endPoint,thickness) {
 var untangleGame = {
 	circles: [],
 	thinLineThickness: 1,
+	boldLineThickness: 5,
 	lines: []
 }
 
@@ -73,6 +74,69 @@ function gameloop() {
 	}
 }
 
+function updateLineIntersection() {
+	// checking lines intersection and bold those lines.
+	for (var i=0;i<untangleGame.lines.length;i++) {
+		for (var j=0;j<i;j++) {
+			var line1 = untangleGame.lines[i];
+			var line2 = untangleGame.lines[j];
+			
+			// we check if two lines are intersected.
+			// and bold the line if they are.
+			if (isIntersect(line1, line2)) {
+				line1.thickness = untangleGame.boldLineThickness;
+				line2.thickness = untangleGame.boldLineThickness;
+			}
+		}
+	}
+}
+
+function isIntersect(line1, line2) {
+	// convert line1 to general form of line: Ax+By = C
+	var a1 = line1.endPoint.y - line1.startPoint.y;
+	var b1 = line1.startPoint.x - line1.endPoint.x;
+	var c1 = a1 * line1.startPoint.x + b1 * line1.startPoint.y;
+	
+	// convert line1 to general form of line: Ax+By = C
+	var a2 = line2.endPoint.y - line2.startPoint.y;
+	var b2 = line2.startPoint.x - line2.endPoint.x;
+	var c2 = a2 * line2.startPoint.x + b2 * line2.startPoint.y;
+	
+	// calculate the intersection point
+	var d = a1*b2 - a2*b1;
+	
+	// parallel when d is 0
+	if (d == 0) {
+		return false;
+	} else {
+		var x = (b2*c1 - b1*c2) / d;
+		var y = (a1*c2 - a2*c1) / d;
+		
+		// check if the intersection point is on both line segments
+		if ((isInBetween(line1.startPoint.x, x, line1.endPoint.x) ||
+			 isInBetween(line1.startPoint.y, y, line1.endPoint.y)) &&
+				(isInBetween(line2.startPoint.x, x, line2.endPoint.x) ||
+				 isInBetween(line2.startPoint.y, y, line2.endPoint.y))) {
+			return true;
+		}
+	}
+	return false;
+}
+
+// return true if b is between a and c,
+// we exclude the result when a==b or b==c
+function isInBetween(a, b, c) {
+	// return false if b is almost equal to a or c.
+	// this is to eliminate some floating point when
+	// two value is equal to each other but different with 0.0000...0001
+	if (Math.abs(a-b) < 0.000001 || Math.abs(b-c) < 0.000001) {
+		return false;
+	}
+	
+	// true when b is in between a and c
+	return (a < b && b < c) || (c < b && b < a);
+}
+
 $(function() {
 	var canvas = document.getElementById("game");
 	var ctx = canvas.getContext("2d");
@@ -91,6 +155,7 @@ $(function() {
 		untangleGame.circles.push(new Circle(x,y,circleRadius)); 
 	}
 	connectCircles();
+	updateLineIntersection();
 	
 	// Add Mouse Event Listener to canvas
 	// we find if the mouse down position is on any circle
@@ -121,6 +186,7 @@ $(function() {
 			untangleGame.circles[untangleGame.targetCircle] = new Circle(mouseX, mouseY, radius);
 		}
 		connectCircles();
+		updateLineIntersection();
 	});
 	
 	// We clear the dragging circle data when mouse is up
